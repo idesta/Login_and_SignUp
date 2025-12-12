2e0f638d20497cd59a17750a49857c4b3cd173b2
idesta <idesta705@gmail.com>
2025-12-12 10:05:57 +0300

    some changes on frontend

--- login_and_signup_client/src/Signup.jsx
+++ login_and_signup_client/src/Signup.jsx
@@ -1,63 +1,65 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Signup(){
     return (
         <div className="d-flex justify-content-center align-items-center bg-secondary vh-100" >
             <div className="bg-white p-3 rounded w-25">
                 <h2>SignUp</h2>
-                <form> 
-                <div className="mb-3">
-                    <label htmlFor="email">
-                        <strong>Name</strong>
-                    </label>
-                    <input
-                        type="text"
-                        placeholder="Enter Name"
-                        autoComplete="off"
-                        name="email"
-                        className="form-control rounded-0"
-                    />
-                </div>
+                <form>
+                    
+                    <div className="mb-3">
+                        <label htmlFor="email">
+                            <strong>Name</strong>
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="Enter Name"
+                            autoComplete="off"
+                            name="email"
+                            className="form-control rounded-0"
+                        />
+                    </div>
 
 
-                <div className="mb-3">
-                    <label htmlFor="email">
-                        <strong>Email</strong>
-                    </label>
-                    <input
-                        type="text"
-                        placeholder="Enter Email"
-                        autoComplete="off"
-                        name="email"
-                        className="form-control rounded-0"
-                    />
-                </div>
+                    <div className="mb-3">
+                        <label htmlFor="email">
+                            <strong>Email</strong>
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="Enter Email"
+                            autoComplete="off"
+                            name="email"
+                            className="form-control rounded-0"
+                        />
+                    </div>
 
-                <div className="mb-3">
-                    <label htmlFor="email">
-                        <strong>Email</strong>
-                    </label>
-                    <input
-                        type="password"
-                        placeholder="Enter Password"
-                        name="password"
-                        className="form-control rounded-0"
-                    />
-                </div>
+                    <div className="mb-3">
+                        <label htmlFor="email">
+                            <strong>Email</strong>
+                        </label>
+                        <input
+                            type="password"
+                            placeholder="Enter Password"
+                            name="password"
+                            className="form-control rounded-0"
+                        />
+                    </div>
 
-                <button type="submit" className="btn btn-success w-100 rounded-0">
-                    SignUp
-                </button>
+                    <button type="submit" className="btn btn-success w-100 rounded-0">
+                        SignUp
+                    </button>
+                </form>
                 <p>Already have an account?</p>
 
-                <button className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">
+                <Link to="/login" className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">
                     Login
-                </button>                                    
-            </form>
+                </Link>                                    
+            </div>
         </div>
-    </div>
- );
+    );
     
 }
 
-export default Signup;
+export default Signup;sssssss
