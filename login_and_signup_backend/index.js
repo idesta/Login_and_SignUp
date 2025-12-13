4a9fb7e811f664eed65a5c63d5308a4e9ac21d1b
idesta <idesta705@gmail.com>
2025-12-13 17:11:00 +0300

    login authentication using page routes

--- login_and_signup_backend/index.js
+++ login_and_signup_backend/index.js
@@ -9,6 +9,22 @@ app.use(cors())
 
 mongoose.connect("mongodb://127.0.0.1:27017/employee");
 
+app.post("/login", (req, res) => {
+    const {email, password} = req.body;
+    EmployeeModel.findOne({email: email})
+    .then(user => {
+        if(user) {
+            if(user.password === password){
+                res.json("Success")
+            }else {
+                res.json("Try again!, Password Incorrect!")
+            }
+        } else {
+            res.json("No record existed")
+        }
+     })
+})
+
 app.post('/register', (req, res) => {
     EmployeeModel.create(req.body)
     .then(employees => res.json(employees))
